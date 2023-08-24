26dc2d07884a2034f3f02ddb34a87b1fa1260b20
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-24 09:13:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import torch
 from seamless_communication.models.inference import Translator
 
 # Initialize a Translator object with a multitask model, vocoder on the GPU.
-translator = Translator("seamlessM4T_medium", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda:0"))
+translator = Translator("seamlessM4T_medium", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda:0"), dtype = torch.float16)
