e5d1e982034884ef089d31d56d7f9c27df40453f
GRATITUD3.ETH <116521143+8gratitude8@users.noreply.github.com>
2023-09-07 17:39:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import torch
 from seamless_communication.models.inference import Translator
 
-replicate_api = st.secrets['REPLICATE_API_TOKEN']
+replicate_api = st.secrets['r8_Cc5VSFUQUSLx8lAew545psbQT1hY8o930tYYb']
 
 # Initialize a Translator object with a multitask model, vocoder on the GPU.
 translator = Translator("seamlessM4T_medium", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda:0"), dtype = torch.float16)
